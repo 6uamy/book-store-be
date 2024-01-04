4ba9a473d51bf68d5bbb28ac061fcaada16048ea
6uamy <hst007@naver.com>
2024-01-04 14:29:56 +0900

    feat: select all category

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
-
 const dotenv = require('dotenv').config();
 
 app.listen(process.env.PORT);
 
 const usersRouter = require('./routes/users');
 const booksRouter = require('./routes/books');
+const categorysRouter = require('./routes/category');
 const cartsRouter = require('./routes/carts');
 const likesRouter = require('./routes/likes');
 const ordersRouter = require('./routes/orders');
 
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
+app.use('/category', categorysRouter);
 app.use('/carts', cartsRouter);
 app.use('/likes', likesRouter);
 app.use('/orders', ordersRouter);
