b2bd7e4dcc1ac4c44190e3e6ddc9f71d6d58451a
6uamy <hst007@naver.com>
2024-01-05 14:56:14 +0900

    feat: paging books list

--- controllers/BooksController.js
+++ controllers/BooksController.js
@@ -2,20 +2,22 @@ const conn = require('../mariadb');
 const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req, res) => {
-    const {category_id, new_book} = req.query;
+    const {category_id, new_book, limit_books, current_page} = req.query;
+    let offset = limit_books * (current_page - 1);
+
     let sql = `SELECT * FROM books`;
-    let values = [];
+    let values = [Number(limit_books), offset];
 
     if (category_id && new_book) {
-        values = [category_id, new_book];
+        values = [category_id, ...values];
         JSON.parse(new_book) === true ? sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()` : sql += ` WHERE category_id = ?`;
     } else if (category_id) {
-        values = category_id;
+        values = [category_id, ...values];
         sql += ` WHERE category_id = ?`;
     } else if (new_book) {
-        values = new_book;
         if (JSON.parse(new_book) === true) sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
     }
+    sql += ` LIMIT ? OFFSET ?`;
 
     conn.query(sql, values, (err, results) => {
         if (err) {
