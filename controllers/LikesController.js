6e2d6d8dc1e52eb1375de72ed6634c0400d13fa7
6uamy <hst007@naver.com>
2024-01-05 15:31:51 +0900

    feat: add likes

---
+++ controllers/LikesController.js
@@ -0,0 +1,24 @@
+const conn = require('../mariadb');
+const {StatusCodes} = require('http-status-codes');
+
+const addLikes = (req, res) => {
+    const {liked_book_id} = req.params;
+    const {user_id} = req.body;
+
+    const sql = `INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)`;
+    const values = [user_id, liked_book_id];
+    conn.query(sql, values, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        return res.status(StatusCodes.CREATED).json(results);
+    });
+}
+
+const removeLikes = (req, res) => {
+    res.json('좋아요 취소');
+}
+
+module.exports = {addLikes, removeLikes};
--- routes/likes.js
+++ routes/likes.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const {addLikes, removeLikes} = require('../controllers/LikesController');
 router.use(express.json());
 
 // 좋아요 추가
-router.post('/:id', (req, res) => {
-    res.json('좋아요 추가');
-});
+router.post('/:liked_book_id', addLikes);
 
 // 좋아요 취소
-router.delete('/:id', (req, res) => {
-    res.json('좋아요 취소');
-});
+router.delete('/:liked_book_id', removeLikes);
 
 module.exports = router;
