ae39dd0b168e5ac9f041c41cdfd3454d7b23fd92
6uamy <hst007@naver.com>
2024-01-09 13:32:26 +0900

    feat: selected cart items

--- controllers/CartsController.js
+++ controllers/CartsController.js
@@ -17,12 +17,19 @@ const addCartsItem = (req, res) => {
 };
 
 const selectCartsItem = (req, res) => {
-    const {user_id} = req.body;
-    const sql = `SELECT C.id, C.book_id, B.title, B.summary, C.quantity, B.price 
+    const {user_id, selected} = req.body;
+    let sql = `SELECT C.id, C.book_id, B.title, B.summary, C.quantity, B.price 
                 FROM cartItems AS C
                 LEFT JOIN books AS B ON C.book_id = B.id 
                 WHERE user_id = ?`;
-    conn.query(sql, user_id, (err, results) => {
+
+    const values = [user_id];
+    if (selected) {
+        sql +=` AND C.id IN (?)`;
+        values.push(selected);
+    }
+    
+    conn.query(sql, values, (err, results) => {
         if (err) {
             console.log(err);
             return res.status(StatusCodes.BAD_REQUEST).end();
@@ -36,6 +43,22 @@ const removeCartsItem = (req, res) => {
     const {id} = req.params;
 
     const sql = `DELETE FROM cartItems WHERE id = ?`;
+    conn.query(sql, id, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        return results.affectedRows === 0 ? res.status(StatusCodes.BAD_REQUEST).end() : res.status(StatusCodes.OK).json(results);
+    });
+};
+
+const choosedCartItems = (req, res) => {
+    const {id} = req.params;
+
+    const sql = `SELECT * FROM Bookshop.cartItems 
+                WHERE user_id = 1
+                AND id IN (4, 6);`;
     conn.query(sql, id, (err, results) => {
         if (err) {
             console.log(err);
--- routes/carts.js
+++ routes/carts.js
@@ -13,9 +13,4 @@ router.get('/', selectCartsItem);
 // 장바구니 도서 삭제
 router.delete('/:id', removeCartsItem);
 
-// 장바구니 주문 예상 상품 목록 조회
-// router.get('/?', (req, res) => {
-//     res.json('장바구니 주문 예상 상품 목록 조회');
-// });
-
 module.exports = router;
